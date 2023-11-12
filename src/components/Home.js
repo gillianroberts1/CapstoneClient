5341929e8213d505e97ec1ef4de6af3968ef59c1
samjaygill <138401809+samjaygill@users.noreply.github.com>
2023-11-12 14:12:52 +0000

    small changes made to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,36 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../context/AuthContext'
-
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import dogs from "./images/assets/dogs.jpeg";
+import "./css/Home.css";
 
 const Home = () => {
-
   const { currentUser } = useContext(AuthContext);
 
   return (
-    <div>
-    <p>Welcome to Wiggle Waggle Waddles, {currentUser && `${currentUser.firstName} ${currentUser.lastName}`}</p>
-    {currentUser && <img src={currentUser.photoURL} alt='' style={{ height: '200px', width: '200px', objectFit: 'cover', borderRadius: '50%'}}/>}<br></br>
-    <button className='enter'>
-        <Link to="/login">Get Started</Link>
-    </button>
-      
-    </div>
-  )
-}
+    <>
+      <div className="home-container">
+        <div className="home-wrapper">
+          <div className="details">
+            <p>
+              Welcome to Wiggle Waggle Waddles,{" "}<br/>
+              {currentUser && `${currentUser.firstName}`}
+            </p>
+            {/* {currentUser && <img src={currentUser.photoURL} alt='' style={{ height: '200px', width: '200px', objectFit: 'cover', borderRadius: '50%'}}/>}<br></br>
+             */}
+          </div>
+          <div className="home-image">
+            <img src={dogs} alt="dogs" />
+          </div>
+        </div>
+      </div>
+      <div className="button">
+        <button className="enter">
+          <Link to="/login">Get Started</Link>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
