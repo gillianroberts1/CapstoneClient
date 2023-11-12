eec475f8367545e11f4173dbdcaa8513ea870f55
samjaygill <138401809+samjaygill@users.noreply.github.com>
2023-11-12 11:45:13 +0000

    members card and css started

--- src/components/members/MemberCard.js
+++ src/components/members/MemberCard.js
@@ -10,10 +10,14 @@ const MemberCard = ({ users }) => {
     return <div>User not found</div>;
   }
 
+  // const toTitleCase = (str) => {
+  //   return str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+  // };
+
   return (
     <div className="member-card-container">
       <div className="member-card-wrapper">
-        <div>
+        <div className="member-card-details">
           <img src={selectedUser.photoURL} alt="user" className="image" />
           <p className="name">
             {selectedUser.firstName} {selectedUser.lastName}
@@ -27,14 +31,47 @@ const MemberCard = ({ users }) => {
             <br />
             Distance walked: {selectedUser.distance} miles
             <br />
-          </div>
-        
-        <button className="invite">
+            <button className="invite">
           <Link to="/walkies">Send Invitation</Link>
         </button>
+          </div>
+          </div>
+          <div className="dog-details">
+          {selectedUser.dogs.map((dog) => (
+            <div className="dog-container">
+            <li className="dogs" key={dog.id}>
+              <Link to={`/dogs/${dog.id}`} className="dog-link">
+                <b>{dog.name}</b><br/>
+                {dog.breed}<br/>
+                Need Leash:{" "}{dog.leash ? (
+              <span>&#10003;</span>
+            ) : (
+              <span>&#10008;</span>
+            )}<br/>
+
+                Neutered:{" "}{dog.neutered ? (
+              <span>&#10003;</span>
+            ) : (
+              <span>&#10008;</span>
+            )}<br/>
+            Vaccinated:{" "}{dog.vaccinated ? (
+              <span>&#10003;</span>
+            ) : (
+              <span>&#10008;</span>
+            )}<br/>
+
+
+
+              </Link>
+            </li>
+            </div>
+          ))}
+          </div>
+          
+        
+        
         </div>
       </div>
-    </div>
   );
 };
 
