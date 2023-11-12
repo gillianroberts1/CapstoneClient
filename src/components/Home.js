b871f779129e5b1b60a95b9f95e9ef7382961146
ebarinia <46579070+ebarinia@users.noreply.github.com>
2023-11-12 11:52:58 +0000

    bug fixes and changed user photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div>
     <p>Welcome to Wiggle Waggle Waddles, {currentUser && `${currentUser.firstName} ${currentUser.lastName}`}</p>
-    {currentUser && <img src={currentUser.photoURL} alt='' style={{ height: '300px', width: '200px'}}/>}<br></br>
+    {currentUser && <img src={currentUser.photoURL} alt='' style={{ height: '200px', width: '200px', objectFit: 'cover', borderRadius: '50%'}}/>}<br></br>
     <button className='enter'>
         <Link to="/login">Get Started</Link>
     </button>
