285a4964f4a3e802564a8534e37c65bd43e16bf3
ebarinia <46579070+ebarinia@users.noreply.github.com>
2023-11-12 21:00:23 +0000

    added notifications

--- src/components/profile/Notification.js
+++ src/components/profile/Notification.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import WalkieCard from '../walkies/WalkieCard'
+import { AuthContext } from '../../context/AuthContext';
 
 const Notification = () => {
+
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser.notifications[0].key)
+
   return (
     <>
-    <div>Notification</div>
+    <div>Notification Centre</div>
+    <p>Location: {currentUser.notifications[0].key}</p>
+    <p>Message: {currentUser.notifications[0].value}</p>
+    <button>View Invitation</button>
     <WalkieCard/>
     </>
 
