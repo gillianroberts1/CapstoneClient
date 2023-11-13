b8639c4d45da10d0705d8d2df8083eddde6a8eca
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-11-13 00:02:35 +0000

    current user comp created

---
+++ src/components/profile/currentUser/CurrentUserDetail.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+
+const CurrentUserDetail = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  //   const location = useLocation();
+  //   const {
+  //     user: { first_name, last_name },
+  //   } = location.state;
+
+  return (
+    <div className="current_user">
+      Current User Detail
+      <p>
+        {currentUser && (
+          <img
+            src={currentUser.photoURL}
+            alt=""
+            style={{
+              height: "200px",
+              width: "200px",
+              objectFit: "cover",
+              borderRadius: "50%",
+            }}
+          />
+        )}
+        <br></br>
+      </p>
+      <p>First Name: {currentUser.firstName}</p>
+      <p>Last Name: {currentUser.lastName}</p>
+      <p>Age: {currentUser.age}</p>
+      <p>Login Details: {currentUser.email}</p>
+      <p>Gender: {currentUser.gender}</p>
+      <p>Home Area: {currentUser.area}</p>
+      <p></p>
+    </div>
+  );
+};
+
+export default CurrentUserDetail;
