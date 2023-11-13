44484fe3c0bda54a31519c64276cd1293149f9c2
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-11-13 00:01:27 +0000

    current user dogs now attached without hardcode

--- src/components/profile/Detail.js
+++ src/components/profile/Detail.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect, useContext } from "react";
 import DogDetail from "./dogs/DogDetail";
-import UserDetail from "./UserDetail";
 import Request from "../../Helpers/Request";
 import { AuthContext } from "../../context/AuthContext";
+import CurrentUserDetail from "./currentUser/CurrentUserDetail";
+
 
 const Detail = ({ selectedOption }) => {
+  const { currentUser } = useContext(AuthContext);
+
   //   const { currentUser } = useContext(AuthContext); currentUser.uid
-  const userId = 1; //logged in user
+  // const userId = 12; //logged in user
   const [user, setUser] = useState();
 
+
   useEffect(() => {
-    const request = new Request();
-    request.get(`/api/users/${userId}`).then((data) => setUser(data));
-  }, [userId]);
+    if (currentUser) {
+      const userId = currentUser.id; 
+      const request = new Request();
+      request.get(`/api/users/${userId}`).then((data) => setUser(data));
+    }
+  }, [currentUser]);
 
   return (
     <>
       <div>
-        {selectedOption === "user" ? <UserDetail /> : null}
-        {selectedOption === "dog" ? <DogDetail dogs={user.dogs} /> : null}
+        {selectedOption === "user" ? <CurrentUserDetail user={user} /> : null}
+        {selectedOption === "dog" ? <DogDetail dogs={user ? user.dogs: []} /> : null}
       </div>
     </>
   );
