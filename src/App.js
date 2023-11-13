e51c7c413f3e36504cc0ba3f0e3b87378b1afbba
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-11-13 20:26:30 +0000

    app styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import NavBar from './components/NavBar';
 import MainContainer from './container/MainContainer';
 import './style.scss'
+import "./App.css"
 
 function App() {
   
   return (
     <>
     <NavBar />
+    <div className='page-content'>
     <MainContainer/>
+    </div>
     </>
   );
 }
