69d4f54e86175579a17c10cdd9fa1e1cb2018fb8
samjaygill <138401809+samjaygill@users.noreply.github.com>
2023-11-14 14:28:55 +0000

    working and styled. added in a navigate

--- src/components/profile/currentUser/CurrentUserForm.js
+++ src/components/profile/currentUser/CurrentUserForm.js
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../../firebase/context/AuthContext";
-import "../css/CurrentUserForm.css"
-import dog from "../../images/assets/dog1.png"
+import "../css/CurrentUserForm.css";
+import dog from "../../images/assets/dog1.png";
+import { useNavigate } from "react-router-dom";
 
 const CurrentUserForm = ({ onUpdateUser }) => {
+  const navigate = useNavigate();
   const { currentUser } = useContext(AuthContext);
 
   const [user, setUser] = useState({
@@ -11,7 +13,6 @@ const CurrentUserForm = ({ onUpdateUser }) => {
     lastName: currentUser.lastName || "",
     age: currentUser.age || null,
     gender: currentUser.gender || null,
-
   });
 
   const handleInputChange = (e) => {
@@ -24,65 +25,73 @@ const CurrentUserForm = ({ onUpdateUser }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Submitting user data:', user);
-    onUpdateUser(user);
+    console.log("Submitting user data:", user);
+    onUpdateUser(user)
+      .then(() => {
+        navigate("/profile");
+      })
+      .catch((error) => {
+        console.error("Error updating user:", error);
+      });
   };
 
   return (
     <div className="userform-container">
-        <div className="dog-card">
-            <img src={dog} alt="dog" className="dog-image"/>
-         </div>
-        <div className="userform-card">
-      <h2>User Update Form</h2>
-      <form onSubmit={handleSubmit}>
-        <label className="label">
-          First Name:{" "}
-          <input
-            type="text"
-            name="firstName"
-            value={user.firstName}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br/>
-        <label className="label">
-          Last Name:{" "}
-          <input
-            type="text"
-            name="lastName"
-            value={user.lastName}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br/>
-        <label className="label">
-          Age:{" "}
-          <input
-            type="number"
-            name="age"
-            value={user.age}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br/>
-        <label className="label">
-          Gender:{" "}
-          <select
-            name="gender"
-            value={user.gender}
-            onChange={handleInputChange}
-          >
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-        </label>
-        <br/>
-        <br/>
-        <button type="submit" className="userform-button">Update User</button>
-      </form>
-    </div>
+      <div className="dog-card">
+        <img src={dog} alt="dog" className="dog-image" />
+      </div>
+      <div className="userform-card">
+        <h2>User Update Form</h2>
+        <form onSubmit={handleSubmit}>
+          <label className="label">
+            First Name:{" "}
+            <input
+              type="text"
+              name="firstName"
+              value={user.firstName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label className="label">
+            Last Name:{" "}
+            <input
+              type="text"
+              name="lastName"
+              value={user.lastName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label className="label">
+            Age:{" "}
+            <input
+              type="number"
+              name="age"
+              value={user.age}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label className="label">
+            Gender:{" "}
+            <select
+              name="gender"
+              value={user.gender}
+              onChange={handleInputChange}
+            >
+              <option value="">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </label>
+          <br />
+          <br />
+          <button type="submit" className="userform-button">
+            Update User
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
